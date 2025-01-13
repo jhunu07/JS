a07e6d2e0e2d950d42198861a25e933a0bff9e61
JHUNNU KUMAR <kumarjhunu7@gmail.com>
2025-01-13 18:30:13 +0000

    addd

--- Basic/First.js
+++ Basic/First.js
@@ -1,1 +1,15 @@
+console.log("hii");
+a=12
+b =11
+if (a>b) {
+    console.log("a");
+    
+} else {
+    console.log("b")
+}
+for (let i = 0; i <=6; i++) {
+   console.log("hiiii");
+   
+    
+}
 
