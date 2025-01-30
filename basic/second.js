3a7a3a0fa721addf790e1f763eb4c060f8e21129
JHUNNU KUMAR <kumarjhunu7@gmail.com>
2025-01-30 12:59:02 +0000

    update

--- basic/second.js
+++ basic/second.js
@@ -6,6 +6,8 @@ const user = {
     }  
 }
 user.welcome()
+// ARROW FUNCTION
+
 // with Parammerter
 var fun=(a,b,c)=>{
 //console.log(a+" "+b+" "+c);
@@ -14,24 +16,34 @@ var fun=(a,b,c)=>{
 fun(12,43,23)
 
 var show=(a,b=80)=>{
-    //console.log(a+" "+b);
-    
+    //console.log(a+" "+b);   
 }
 show( "mango ","apple")
 
 // without parenthese
- 
 var car=x=>{
-    //console.log(x);
-    
+    //console.log(x);    
 }
 car("maruti")
 // Arrow function
 var show = value => value/2;  
-console.log(show(100)); 
+//console.log(show(100)); 
 // Normal function
 function myfun(value){
 return value/2
 }
-console.log(myfun(1000));
+//console.log(myfun(1000));
  
+// IIFE  IMMEDIATELY INVOKED FUNCTION EXPRESSION
+(function (){
+    console.log("connected");
+
+})();
+
+// by arrow function
+( (a,b,c)=>{
+    console.log(a*b*c);
+
+})(2,3,4)
+
+
